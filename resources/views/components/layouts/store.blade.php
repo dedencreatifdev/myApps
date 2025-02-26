6ba728dc0e3ad549573416ddc4d5e091ba0f429e
dedencreatifdev <dedencreatifdeveloper@gmail.com>
2025-02-26 17:07:49 +0700

    .

---
+++ resources/views/components/layouts/store.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.store.header>
+    <flux:main>
+        {{ $slot }}
+    </flux:main>
+</x-layouts.store.header>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
         <div class="grid auto-rows-min gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6">
 
 
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+            <div class="relative aspect-square overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
@@ -28,7 +28,7 @@
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
             </div>
-
+{{--  --}}
 
         </div>
         <div class="relative h-full flex-1 overflow-hidden rounded-md border border-neutral-200 dark:border-neutral-700">
